1e9cbfd9e1d0277e5e501cd9876b9515cec23dae
irakli1990 <iraklikardawa@gmail.com>
2020-12-03 22:16:46 +0100

    implementation of knn euclidean distance

--- main.py
+++ main.py
@@ -1,16 +1,45 @@
-# This is a sample Python script.
+import pandas as pd
+from math import sqrt
+import matplotlib.pyplot as plt
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+PATH_DATASET_EXEL = 'data/dataset.xlsx'
+PATH_DATASET_CSV = 'data/dataset.csv'
 
 
-# Press the green button in the gutter to run the script.
+def load_csv(path):
+    read_file = pd.read_excel(path, 1)
+    read_file.to_csv(PATH_DATASET_CSV, index=None, header=True)
+    df = pd.DataFrame(pd.read_csv(PATH_DATASET_CSV))
+    return df
+
+
+def euclidean_distance(row1, row2):
+    distance = 0.0
+    for i in range(len(row1) - 1):
+        distance += (row1[i] - row2[i]) ** 2
+    return sqrt(distance)
+
+
+def get_neighbors(train, test_row, num_neighbors):
+    distances = list()
+    for train_row in train:
+        dist = euclidean_distance(test_row, train_row)
+        distances.append((train_row, dist))
+    distances.sort(key=lambda tup: tup[1])
+    neighbors = list()
+    for i in range(num_neighbors):
+        neighbors.append(distances[i][0])
+    return neighbors
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    dataframe = load_csv(PATH_DATASET_EXEL)
+    print(dataframe.columns.values)
+    print(dataframe.values)
+    dataset = dataframe.iloc[:, [0, 1, 2, 3]].values
+    print(euclidean_distance(dataset[0], dataset[1]))
+    neighbors = get_neighbors(dataset, dataset[0], 5)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    for neighbor in neighbors:
+        print(neighbor)
+    plt.show()
